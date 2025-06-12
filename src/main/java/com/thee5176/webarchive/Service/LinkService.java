112f87c7a3bb000f40221a883518d3e3e2eb75cc
Thee5176 <Theerapong5176@gmail.com>
2025-06-12 16:53:04 +0900

    Fix style link service

--- src/main/java/com/thee5176/webarchive/Service/LinkService.java
+++ src/main/java/com/thee5176/webarchive/Service/LinkService.java
@@ -11,6 +11,7 @@ import com.thee5176.webarchive.dto.LinkDTO;
 import com.thee5176.webarchive.model.Link;
 import com.thee5176.webarchive.model.Tag;
 
+import jakarta.transaction.Transactional;
 import mapper.LinkDTOMapper;
 
 @Service
@@ -20,11 +21,12 @@ public class LinkService {
 
 	@Autowired
 	TagRepository tagRepository;
-	
+
+	@Transactional
 	public Link createLinkWithTag(LinkDTO linkDto) throws RuntimeException {
-		
+
 		// Check if link with the same name already exists
-		if ( !linkRepository.existsByName(linkDto.name()) ) {
+		if (!linkRepository.existsByName(linkDto.name())) {
 			// Check if tag exist in the database
 			Tag tag = tagRepository.findById(linkDto.tagId())
 					.orElseThrow(() -> new RuntimeException("Tag with id " + linkDto.tagId() + " not created yet"));
@@ -34,18 +36,19 @@ public class LinkService {
 			link.setUpdatedAt(Instant.now());
 			// Set the tag for the link entity
 			link.setTag(tag);
-			
+
 			return linkRepository.saveAndFlush(link);
-			
+
 		} else {
 			throw new RuntimeException("Bookmark already created");
 		}
 	}
 	
-public Link updateLinkWithTag(LinkDTO linkDto) throws RuntimeException {
-		
+	@Transactional
+	public Link updateLinkWithTag(LinkDTO linkDto) throws RuntimeException {
+
 		// Check if link with the same name already exists
-		if ( !linkRepository.existsByName(linkDto.name()) ) {
+		if (!linkRepository.existsByName(linkDto.name())) {
 			// Check if tag exist in the database
 			Tag tag = tagRepository.findById(linkDto.tagId())
 					.orElseThrow(() -> new RuntimeException("Tag with id " + linkDto.tagId() + " not created yet"));
@@ -54,9 +57,9 @@ public Link updateLinkWithTag(LinkDTO linkDto) throws RuntimeException {
 			link.setUpdatedAt(Instant.now());
 			// Set the tag for the link entity
 			link.setTag(tag);
-			
+
 			return linkRepository.saveAndFlush(link);
-			
+
 		} else {
 			throw new RuntimeException("Bookmark already created");
 		}
