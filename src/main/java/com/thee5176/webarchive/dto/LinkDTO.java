ee21903b61c033e6a3d590307349aac1c2fbbe62
Thee5176 <Theerapong5176@gmail.com>
2025-06-15 01:32:19 +0900

    change error message

--- src/main/java/com/thee5176/webarchive/dto/LinkDTO.java
+++ src/main/java/com/thee5176/webarchive/dto/LinkDTO.java
@@ -7,19 +7,18 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 public record LinkDTO(
-	@NotEmpty(message = "サイト名：入力してください！")
-	@Size(min=2, max=15, message="タッグ名：2-15文字を抑えてください！")
+	@NotEmpty(message = "サイト名:入力してください")
+	@Size(min=2, max=15, message="サイト名:2-15文字を抑えてください")
 	String name,
 	
-	@NotEmpty(message = "リンク：URLを入力してください！")
+	@NotEmpty(message = "リンク:URLを入力してください")
 	@Website
 	String url,
 	
-	@Size(min=2, max=50, message="詳細：2-50文字を抑えてください！")
-	@Website
+	@Size(min=2, max=50, message="詳細:2-50文字を抑えてください")
 	String description,
 	
-	@Min( value = 0L, message = "タッグ：選択してください！")
+	@Min( value = 0L, message = "タッグ:選択してください")
 	long tagId
 ) {
 	
